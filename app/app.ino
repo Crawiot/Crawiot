010b083a8bfe83c16653dcc9f408cbbb91946cb2
bymse <egorstrelov@gmail.com>
2021-10-09 22:50:24 +0300

    Initial app

---
+++ app/app.ino
@@ -0,0 +1,9 @@
+#include "crawiot_app.h"
+
+void setup() {
+    crawiot_setup();
+}
+
+void loop() {
+    crawiot_loop();
+}
---
+++ app/crawiot_app.h
@@ -0,0 +1,7 @@
+#ifndef APP_CRAWIOT_LIBRARY_H
+#define APP_CRAWIOT_LIBRARY_H
+
+void crawiot_setup();
+void crawiot_loop();
+
+#endif //APP_CRAWIOT_LIBRARY_H
