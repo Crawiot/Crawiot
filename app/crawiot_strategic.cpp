fb87378f8c82fd0a9638abc9542eb8b71ec33a6b
bymse <egorstrelov@gmail.com>
2021-10-10 22:22:00 +0300

    11 Strategic module

---
+++ app/crawiot_strategic.cpp
@@ -0,0 +1,11 @@
+#include "crawiot_strategic.h"
+
+Strategic StrategicModule = Strategic();
+
+void Strategic::reach_target() {
+    
+}
+
+void Strategic::reach_coordinates(const Coordinates coordinates) {
+    
+}
