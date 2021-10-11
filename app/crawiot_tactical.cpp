f3afb30aa53fc55bc7fdd4ddca127128bc8f8153
bymse <egorstrelov@gmail.com>
2021-10-11 21:35:10 +0300

    17 Tactical module

--- app/crawiot_tactical.cpp
+++ app/crawiot_tactical.cpp
@@ -1,14 +1,25 @@
 #include "crawiot_tactical.h"
 #include "Arduino.h"
 #include "crawiot_traces.h"
+#include "crawiot_mediator.h"
 
-void Tactical::reach_target(const Coordinates target_coordinates[], int count) {
+Tactical TacticalModule = Tactical();
 
+[[noreturn]] void Tactical::task(void *pvParameters) {
+    while (1) {
+        Coordinates cords;
+        
+        const bool was_pulled = ModulesMediator.pull_sub_target(&cords);
+        if (was_pulled) {
+            TacticalModule.reach_target(cords);
+        }
+        
+        delay(500);
+    }
 }
 
-void Tactical::task(void *pvParameters) {
-    while (1) {
-        GlobalTracer.send_trace("Tactical module is running");
-        delay(1000);
-    }
+void Tactical::reach_target(const Coordinates &target_coordinates) {
+    String empty = "TacticalModule. Reaching: ";
+    empty.concat(target_coordinates.X);
+    GlobalTracer.send_trace(empty);
 }
