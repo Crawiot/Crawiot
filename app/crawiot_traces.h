c4f2ecb5c926e5e0c576a28501787bc1b0f6201d
Nikita Romanov <nik-romanof@yandex.ru>
2021-10-15 21:24:29 +0300

    9 buffer added to traces

--- app/crawiot_traces.h
+++ app/crawiot_traces.h
@@ -3,11 +3,26 @@
 
 #include <Arduino.h>
 
+const int traceBufferLength = 100;
+
+class CircularBuffer {
+public:
+    void add(String message);
+
+private:
+    int recordIndex = 0;
+    int currentBufferSize = 0;
+    String buffer[traceBufferLength];
+};
+
 class Tracer {
 public:
     void setup();
     void send_trace(String message);
     void light_default_led();
+
+private:
+    CircularBuffer log;
 };
 
 extern Tracer GlobalTracer;
