38a218dcebac0fc8a6d7172fca4dbc393242debc
bymse <egorstrelov@gmail.com>
2021-10-16 21:47:35 +0300

    13 Enable wi-fi

--- app/crawiot_network_wifi.cpp
+++ app/crawiot_network_wifi.cpp
@@ -1,7 +1,17 @@
 #include "crawiot_network.h"
 #include "crawiot_traces.h"
+#include <WiFi.h>
 
-bool Network::enable_wifi() {
+bool Network::enable_wifi(const char *ssid, const char *passphrase) {
+    WiFi.mode(WIFI_AP);
+
+    if (!WiFi.softAP(ssid, passphrase)) {
+        GlobalTracer.send_trace("Failed to start Wi-fi");
+        return false;
+    }
+    
+    WiFi.softAPConfig(this->ipAddress, ipAddress, IPAddress(255, 255, 255, 0));
+
     GlobalTracer.send_trace("Wi-fi enabled");
     return true;
 }
