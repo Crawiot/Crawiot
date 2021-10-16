9de79949d335778179f2485edf18934e39caa4f6
bymse <egorstrelov@gmail.com>
2021-10-16 21:49:07 +0300

    13 Static task -> lambda

--- app/crawiot_network.h
+++ app/crawiot_network.h
@@ -6,17 +6,17 @@
 class Network {
 
 public:
-    [[noreturn]] static void task(void *pvParameters);
-
     void setup();
 
     void task();
 
 private:
     bool enable_wifi(const char *ssid, const char *passphrase);
-    bool enable_dns(const char* hostName);
+
+    bool enable_dns(const char *hostName);
+
     bool start_http_server();
-    
+
     DNSServer dnsServer;
     
     IPAddress ipAddress = IPAddress().fromString("192.168.4.1");
