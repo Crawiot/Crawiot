0512a5a2a23906b88f94339ced142decaafe89d2
EgoInc <56895607+EgoInc@users.noreply.github.com>
2021-10-20 15:42:36 +0300

    Update crawiot_motion.cpp

--- app/crawiot_motion.cpp
+++ app/crawiot_motion.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "crawiot_motion.h"
+#include <Servo.h>
+
+static const int servoPin = 13;
+static const int degree = 90; //Для начала какой-нить рандомный угол поворота
+Servo servo1;
+
+void setup() {
+    Serial.begin(115200);
+    servo1.attach(servoPin);
+}
+void startRotate(int degree){
+  for(int posDegrees = 0; posDegrees <= degree; posDegrees++) {
+        servo1.write(posDegrees);
+        delay(40); //Скорость вращения если что поменяем тут
+    }
+}
+
+void stopRotate(int degree){
+  for(int posDegrees = degree; posDegrees >= 0; posDegrees--) {
+        servo1.write(posDegrees);
+        delay(40);
+    }
+}
+
+
+
+void loop() { //Она пока будет крутиться до конца, немного ждать и топать обратно
+  startRotate(degree);
+  delay(1000);
+  stopRotate(degree);
+  
+}
