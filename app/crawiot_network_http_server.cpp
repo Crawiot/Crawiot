ef4b5b47a277759f23a5618bde6018425c8ec813
bymse <egorstrelov@gmail.com>
2021-10-22 16:46:55 +0300

    18 POST target endpoint impl

--- app/crawiot_network_http_server.cpp
+++ app/crawiot_network_http_server.cpp
@@ -1,17 +1,42 @@
 #include "crawiot_network.h"
 #include "crawiot_traces.h"
 #include "crawiot_main_page.h"
+#include "HTTP_Method.h"
+#include "crawiot_location.h"
+#include "crawiot_mediator.h"
 
 WebServer webServer(80);
 
 bool Network::start_http_server() {
 
-    webServer.on(("/"), [] () {
+    webServer.on(("/"), []() {
         webServer.send(200, "text/html", CrawiotIndexPage);
     });
-    webServer.begin();
-    
-    GlobalTracer.send_trace("HTTP server started");
 
+    webServer.on("/api/target", HTTP_POST, []() {
+        const int x = webServer.arg("X").toInt();
+        const int y = webServer.arg("Y").toInt();
+        const Coordinates coordinates = {
+                .X = x,
+                .Y = y
+        };
+        
+        const bool wasPushed = ModulesMediator.push_target(coordinates);
+        String locationHeaderValue = "/?X=";
+        locationHeaderValue.concat(x);
+        
+        locationHeaderValue.concat("&Y=");
+        locationHeaderValue.concat(y);
+
+        locationHeaderValue.concat("&success=");
+        locationHeaderValue.concat(wasPushed ? "true" : "false");
+        
+        webServer.sendHeader("location", locationHeaderValue);
+        webServer.send(302);
+    });
+
+
+    webServer.begin();
+    GlobalTracer.send_trace("HTTP server started");
     return true;
 }
