f1893d004af2885ded0426fe63fcbecd4a20d336
bymse <egorstrelov@gmail.com>
2021-10-30 21:19:54 +0300

    Revert "Servo to simple gpio"
    This reverts commit 421f9efb5feb0b2fdc42aaf8bfd6069545a52245.

--- app/crawiot_motion.cpp
+++ app/crawiot_motion.cpp
@@ -10,8 +10,7 @@ void Motion::setup(const Config &config) {
     this->tracker.setup(config);
     
     this->firstSpeedDegree = config.FirstSpeedDegree;
-    pinMode(SERVO_PIN_D13, OUTPUT);
-    //this->servo.attach(SERVO_PIN_D13);
+    this->servo.attach(SERVO_PIN_D13);
     this->stopRotate();
     
 }
@@ -35,14 +34,12 @@ void Motion::startRotate(int degree) {
     if (this->currentDegree == degree){
         return;
     }
-
-    digitalWrite(SERVO_PIN_D13, HIGH);
-    //this->servo.write(degree);
+    
+    this->servo.write(degree);
     this->currentDegree = degree;
 }
 
 void Motion::stopRotate(){
-    digitalWrite(SERVO_PIN_D13, LOW);
-//    this->servo.write(0);
+    this->servo.write(0);
     this->currentDegree = 0;
 }
