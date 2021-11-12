353952d10e2be53461de2b6040e0c344278cfc68
Nikita Romanov <nik-romanof@yandex.ru>
2021-11-12 18:37:03 +0300

    41 Update old version of  module

--- backend/find_path.py
+++ backend/find_path.py
@@ -1,12 +1,39 @@
 from typing import List, Tuple
+from queue import Queue
+
 import sympy.geometry as geoma
 import itertools as itool
 
-from sympy.geometry.line import Segment2D 
 
-def get_path(map) -> List[Tuple[float]]:
-    start_pos = map.start_pos
-    end_pos = map.end_pos
+def bfs(graph, n, s, t):
+    q = Queue(maxlen=100000)
+    q.put(s)
+    used = [False for _ in range(n)]
+    dist = [0 for _ in range(n)]
+    pred = [0 for _ in range(n)]
+    used[s] = True
+    pred[s] = -1
+    while not q.empty():
+        v = q.get()
+        for i in range(len(graph[v])):
+            to = graph[v][i]
+            if not used[to]:
+                used[to] = True
+                q.put(to)
+                dist[to] = dist[v] + 1
+                pred[to] = v
+    if not used[to]:
+        return []
+    else:
+        path = []
+        v = t
+        while v != s:
+            path.append(v)
+            v = pred[v]
+        return path[::-1]
+
+def get_path(start_pos: Tuple[float], end_pos: Tuple[float], barriers: List[Tuple[Tuple[float]]], 
+             borders: List[Tuple[Tuple[float]]]) -> List[Tuple[float]]:
 
     rays = []
 
@@ -23,7 +50,7 @@ def get_path(map) -> List[Tuple[float]]:
     add_rays(end_pos, (1, 1), (-1, -1))
 
     segments = []
-    for x1, y1, x2, y2 in itool.chain(map.barriers, map.borders):
+    for x1, y1, x2, y2 in itool.chain(barriers, borders):
         if x1 > x2:
             x1, x2 = x2, x1
         if y1 > y2:
@@ -39,14 +66,14 @@ def get_path(map) -> List[Tuple[float]]:
         segments.append(geoma.Segment2D(pt3, pt1))
         segments.append(geoma.Segment2D(pt2, pt1))
 
-        if (x1, y1, x2, y2) in map.borders:
+        if (x1, y1, x2, y2) in borders:
             continue
 
         for pt, dir in zip([pt1, pt2, pt3, pt4], [(-1, -1), (-1, 1), (1, -1), (1, 1)]):
             add_rays(pt, dir)
 
     nodes = [start_pos, end_pos]
-    def add_intersections(ray: geoma.Ray2D):
+    def intersections(ray: geoma.Ray2D, is_void=False):
         pts = []
         for seg in segments:
             if len(seg.intersection(ray)) == 0:
@@ -54,23 +81,30 @@ def get_path(map) -> List[Tuple[float]]:
             pts.append(seg.intersection(ray))
         if len(pts) < 2:
                 return 0
-        minv = pts[0]
-        for pt in pts:
-            if ray.p1.distance(pt) < ray.p1.distance(minv):
-                minv = pt
-        nodes.append(minv)
+        if not is_void and isinstance(ray, geoma.Ray2D):
+            minv = pts[0]
+            for pt in pts:
+                if ray.p1.distance(pt) < ray.p1.distance(minv):
+                    minv = pt
+            nodes.append(geoma.Segment2D(minv, ray.p1).midpoint)
         return len(pts)
 
     for ray in rays:
-        add_intersections(ray)
+        intersections(ray)
 
-    
+    mappa = dict()
+    for i in range(len(nodes)):
+        mappa[nodes[i]] = i
 
-        '''
-        TODO:
-        1. Добавить пересечения с лучами из start_pos, end_pos
-        2. Добавить границу чтобы с ней мы тоже пересекались если что
-        3. Перебрать ноды и посоединять их
-        4. Дейкстра на графе с сохранением пути (отдельной функцией)
-        5. Вернуть собственно путь (почему это вообще отдельны пункт)
-        '''
+    graph = [[] for i in range(len(nodes))]
+    for i in range(len(nodes)):
+        for j in range(i):
+            seg = geoma.Segment2D(nodes[i], nodes[j])
+            if intersections(seg, is_void=True) != 0:
+                continue
+            graph[mappa[nodes[i]]].append(mappa[nodes[j]])
+            graph[mappa[nodes[j]]].append(mappa[nodes[i]])
+
+    path = bfs(graph, len(nodes), mappa[start_pos], mappa[end_pos])
+    path = list(map(lambda x: mappa[x], path))
+    return path
