c78b840201ba3231db18576dac814a81fbe83134
EgoInc <56895607+EgoInc@users.noreply.github.com>
2021-11-13 01:11:30 +0300

    Update crawiot_network_http_server.cpp

--- app/crawiot_network_http_server.cpp
+++ app/crawiot_network_http_server.cpp
@@ -5,27 +5,19 @@
 #include "crawiot_mediator.h"
 #include "ArduinoJson.hpp"
 
-using namespace ARDUINOJSON_NAMESPACE;
+ARDUINOJSON_NAMESPACE::StaticJsonDocument<1024> doc;
 
 WebServer webServer(80);
-StaticJsonDocument<1024> doc;
-
-
 void handlePostSubtargetsRequest();
-
 void handleGetTracesRequest();
 void handleGetLocationRequest();
-
-
 void clearDocAndBadRequest();
 
 bool Network::startHttpServer() {
 
     webServer.on("/api/subtargets", HTTP_POST, handlePostSubtargetsRequest);
-
     webServer.on("/api/traces", handleGetTracesRequest);
     webServer.on("/api/location", handleGetLocationRequest);
-
     webServer.begin();
     GlobalTracer.sendTrace("HTTP server started");
     return true;
@@ -42,7 +34,7 @@ void handlePostSubtargetsRequest() {
     }
 
     const auto size = array.size();
-    auto *subtargets = new Coordinates[size];
+    auto* subtargets = new Coordinates[size];
     for (size_t index = 0; index < size; index++) {
         subtargets[index] = {
                 .X = array.getElement(index)["x"].as<float>(),
@@ -60,7 +52,8 @@ void handlePostSubtargetsRequest() {
     doc.clear();
     if (wasPushed) {
         webServer.send(200);
-    } else {
+    }
+    else {
         webServer.send(500);
     }
 }
@@ -73,18 +66,13 @@ void clearDocAndBadRequest() {
 void handleGetTracesRequest() {
     webServer.send(200, "text/plain", GlobalTracer.getTraces());
 }
+
 void handleGetLocationRequest() {
-    //1) Взять локацию из GLM
-    const int X = GlobalLocationManager.currentLocation.X; 
-    const int Y = GlobalLocationManager.currentLocation.Y;
-    //2) ArduinoJSON создать объект с ХУ, синхронз с GLM
-    const size_t CAPACITY = JSON_OBJECT_SIZE(1);
-    StaticJsonDocument<CAPACITY> doc;
-    JsonObject jsonDoc = doc.to<JsonObject>();
+    ARDUINOJSON_NAMESPACE::StaticJsonDocument<64> bodyDoc;
+    auto jsonDoc = bodyDoc.to<ArduinoJson::JsonObject>();
     jsonDoc["X"] = GlobalLocationManager.currentLocation.X;
     jsonDoc["Y"] = GlobalLocationManager.currentLocation.Y;
-    //3) 
-    string body = "";
+    String body = "";
     serializeJson(jsonDoc, body);
     webServer.send(200, "application/json", body);
 }
