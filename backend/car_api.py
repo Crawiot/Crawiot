e5289c11e971ac50ecea4e0b439a7ed618bf4524
me-eh <76014755+me-eh@users.noreply.github.com>
2021-11-12 22:34:01 +0300

    40 interaction with the car api

--- backend/car_api.py
+++ backend/car_api.py
@@ -1,9 +1,21 @@
 from typing import List
+import requests
+import json
 
+def post_target(list) -> bool:
 
-def post_target(x: int, y: int) -> bool:
+    data = {'subtargets': list}
+    headers = {'Content-type': 'application/json'}
+    r = requests.post("http://crawiot.lan/api/subtargets", data=json.dumps(data), headers=headers)
     return True
 
-
 def get_traces() -> str:
-    return 'trace1\r\ntrace2'
+    trace = requests.get("http://crawiot.lan/api/traces")
+    return trace.text()
+
+def get_location() -> tuple:
+    location = requests.get("http://crawiot.lan/api/location")
+    location.json()
+    parsed_string = json.loads(location.json())
+    coordinates = (parsed_string["x"], parsed_string["y"])
+    return coordinates
